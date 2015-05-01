802e6cb01ef17743d9e4e517b64511ec30cde0d3
Steve <steve.schneider99@gmail.com>
2015-05-01 10:56:50 -0700

    add app.rb code for index and post. add code for index.erb for form and listing current words. add first line into definition.erb

--- app.rb
+++ app.rb
@@ -5,5 +5,13 @@ require('./lib/word')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @words = Word.all()
   erb(:index)
+end
+
+post('/definitions') do
+  word = params.fetch('new_word')
+  Word.new(word).save()
+  @word = word
+  erb(:definition)
 end
